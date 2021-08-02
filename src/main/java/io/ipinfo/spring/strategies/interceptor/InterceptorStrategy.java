ce856d48b9e01cde23599c070c984c40d838f7d1
Uman Shahzad <uman@ipinfo.io>
2021-08-02 06:52:45 +0500

    basic fixes to strats

--- src/main/java/io/ipinfo/spring/strategies/interceptor/InterceptorStrategy.java
+++ src/main/java/io/ipinfo/spring/strategies/interceptor/InterceptorStrategy.java
@@ -3,6 +3,5 @@ package io.ipinfo.spring.strategies.interceptor;
 import javax.servlet.http.HttpServletRequest;
 
 public interface InterceptorStrategy{
-
     boolean shouldRun(HttpServletRequest request);
 }
